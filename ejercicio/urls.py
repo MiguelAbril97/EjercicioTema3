b20ecf7d103f03fa84949ee68821cd6bd158d3e4
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-24 13:00:27 +0200

    buscar por observacion

--- ejercicio/urls.py
+++ ejercicio/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('proyectos/listar',views.listar_proyectos,name='listar_proyectos'),
     path('tareas/<int:id_proyecto>/',views.tareas_proyecto,name="tareas_proyecto"),
+    path('usuarios/<int:id_tarea>/',views.tarea_usuarios,name="tarea_usuarios"),
+    path('tareas/<str:observacion>/',views.tareas_observacion,name="tareas_observacion"),
 ]
--- ejercicio/views.py
+++ ejercicio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q,F,Prefetch
+from django.db.models import Avg,Max,Min
 # Create your views here.
 def index(request):
     return render(request, 'index.html') 
@@ -12,5 +14,19 @@ def listar_proyectos(request):
 def tareas_proyecto(request,id_proyecto):
     proyecto = Proyecto.objects.get(id=id_proyecto)
     tareas = Tarea.objects.select_related('creador','proyecto').prefetch_related('usuario')
-    tareas = tareas.filter(proyecto=id_proyecto)
+    tareas = tareas.filter(proyecto=id_proyecto).order_by('-fecha_creacion')
     return render (request,'tareas/lista.html', {'tareas_mostrar':tareas,"proyecto":proyecto})
+    """
+    Crear una URL que muestre todos los usuarios que están asignados
+    a una tarea ordenados por la fecha de asignación 
+    de la tarea de forma ascendente. 
+    """
+def tarea_usuarios(request, id_tarea):
+    usuarios = Asignacion.objects.select_related('tarea').select_related('usuario')
+    usuarios = usuarios.filter(tarea=id_tarea).order_by('fecha_asignacion')
+    return render (request, 'tareas/usuarios.html', {'tarea_usuarios':usuarios})
+
+def tareas_observacion (request, observacion):
+    tareas = Tarea.objects.select_related('creador', 'proyecto').prefetch_related('usuario')
+    tareas = tareas.filter(observaciones=observacion)
+    return render(request, 'tareas/lista.html',{'tareas_mostrar':tareas})
