5513c28c813b62887a609d1bc6b117ee9d590398
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-24 10:48:45 +0200

    creo tareas_proyecto y html tareas lista

--- ejercicio/urls.py
+++ ejercicio/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('proyectos/listar',views.listar_proyectos,name='listar_proyectos'),
+    path('tareas/<int:id_proyecto>/',views.tareas_proyecto,name="tareas_proyecto"),
 ]
--- ejercicio/views.py
+++ ejercicio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Proyecto, Usuario
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html') 
@@ -8,3 +8,9 @@ def listar_proyectos(request):
     proyectos = Proyecto.objects.select_related('administrador').prefetch_related('colaboradores')
     proyectos = proyectos.all()
     return render(request, 'proyectos/lista.html', {'proyectos_mostrar':proyectos})
+
+def tareas_proyecto(request,id_proyecto):
+    proyecto = Proyecto.objects.get(id=id_proyecto)
+    tareas = Tarea.objects.select_related('creador','proyecto').prefetch_related('usuario')
+    tareas = tareas.filter(proyecto=id_proyecto)
+    return render (request,'tareas/lista.html', {'tareas_mostrar':tareas,"proyecto":proyecto})
